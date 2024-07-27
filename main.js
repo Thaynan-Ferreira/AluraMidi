64201dde2b33e6750f30469474b65dc255ee7084
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2024-07-27 09:03:11 -0300

    Todas as teclas funcionando

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador]; //guarda quais são as teclas que podem ser clicadas
+    const instrumento = tecla.classList[1];//guarda quais são os adios disponiveis, usando o classe unica de cada tag audio
+    const idAudio = `#som_${instrumento}`;//guarda o ID necessario pra acessar cada audio
+
+    tecla.onclick = function() { // função Anonima
+        tocaSom(idAudio);
+    }
+
+    contador += 1;
+}
